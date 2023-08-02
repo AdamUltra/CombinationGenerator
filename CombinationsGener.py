407b921a8687b6603161f9b900a4366cbdc4ad98
PythJavHacker <adam.eldesoky@gmail.com>
2023-08-02 23:56:25 +0300

    added a loop

--- CombinationsGener.py
+++ CombinationsGener.py
@@ -1,26 +1,39 @@
 import random
-stop = 0
-start = 1
-numbers = list(input('enter numbers you want to know the number of their possible combinations'))
-poss_comb = [map(list, numbers)]
-all_comb = (map(str, numbers))
-all_comb = ''.join(all_comb)
 
-while start == 1:
-    random.shuffle(numbers)
-    comb = (map(str, numbers))
-    comb = ''.join(comb)
-    if comb not in all_comb:
-        stop = 0
-        poss_comb.append((map(str, numbers)))
-        all_comb = all_comb + ', ' + comb
 
-    elif stop < 50000:
-        stop += 1
+def gener():
+    stop = 0
+    start = 1
+    numbers = list(input('enter numbers you want to know the number of their possible combinations'))
+    poss_comb = [map(list, numbers)]
+    all_comb = (map(str, numbers))
+    all_comb = ''.join(all_comb)
+
+    while start == 1:
+        random.shuffle(numbers)
+        comb = (map(str, numbers))
+        comb = ''.join(comb)
+        if comb not in all_comb:
+            stop = 0
+            poss_comb.append((map(str, numbers)))
+            all_comb = all_comb + ', ' + comb
+
+        elif stop < 50000:
+            stop += 1
+
+        else:
+            start = 0
+
+    print('Number of possible combinations: ', len(poss_comb))
+    if len(all_comb) < 501:
+        print('The combinations: ', all_comb)
+
+    UInp = input('Do you want to enter another sequence?')
+    if UInp == 'yes':
+        gener()
 
     else:
-        start = 0
+        quit()
 
-print('Number of possible combinations: ', len(poss_comb))
-if len(all_comb) < 501:
-    print('The combinations: ', all_comb)
+
+gener()
